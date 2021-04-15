aab42c73d0fbe28b71a4308304a22b7cc60392fe
Tobias Langendorf <junglerobba@jngl.one>
2021-04-16 00:31:04 +0200

    screenshot: implement `interactive` option

--- src/screenshot/screenshot.c
+++ src/screenshot/screenshot.c
@@ -35,17 +35,144 @@ static bool exec_screenshooter(const char *path) {
 
 	return stat == 0;
 }
+static bool exec_screenshooter_with_coordinates(const char *path,
+		char *coords) {
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("fork");
+		return false;
+	} else if (pid == 0) {
+		char cmd[strlen(coords) + strlen(path) + 14];
+		sprintf(cmd, "grim -g \"%s\" -- %s", coords, path);
+		execl("/bin/sh", "/bin/sh", "-c", cmd, NULL);
+		perror("execl");
+		exit(127);
+	}
+
+	int stat;
+	if (waitpid(pid, &stat, 0) < 0) {
+		perror("waitpid");
+		return false;
+	}
+
+	return stat == 0;
+}
+
+static bool spawn_chooser(int chooser_out[2]) {
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("fork");
+		return false;
+	} else if (pid == 0) {
+		close(chooser_out[0]);
+
+		dup2(chooser_out[1], STDOUT_FILENO);
+		close(chooser_out[1]);
+
+		execl("/bin/sh", "/bin/sh", "-c", "slurp", NULL);
+
+		perror("execl");
+		_exit(127);
+	}
+
+	int stat;
+	if (waitpid(pid, &stat, 0) < 0) {
+		perror("waitpid");
+		return false;
+	}
+
+	close(chooser_out[1]);
+	return stat == 0;
+}
+
+static char *exec_coordinates_selector() {
+	size_t size = 0;
+
+	int chooser_out[2];
+	if (pipe(chooser_out) == -1) {
+		perror("pipe chooser_out");
+		return false;
+	}
+
+	if (!spawn_chooser(chooser_out)) {
+		logprint(ERROR, "Region selection failed");
+		close(chooser_out[0]);
+		return false;
+	}
+
+	FILE *f = fdopen(chooser_out[0], "r");
+	if (f == NULL) {
+		perror("fopen pipe chooser_out");
+		close(chooser_out[0]);
+		return false;
+	}
+
+	char *result;
+	ssize_t nread = getline(&result, &size, f);
+	fclose(f);
+	close(chooser_out[0]);
+	if (nread < 0) {
+		perror("getline failed");
+		return false;
+	}
+	char *coordinates = strtok(result, "\n");
+	if (coordinates == NULL) {
+		perror("Failed to get coordinates");
+		return false;
+	}
+	return coordinates;
+}
 
 static int method_screenshot(sd_bus_message *msg, void *data,
 		sd_bus_error *ret_error) {
 	int ret = 0;
+
+	bool interactive = false;
 
 	char *handle, *app_id, *parent_window;
 	ret = sd_bus_message_read(msg, "oss", &handle, &app_id, &parent_window);
 	if (ret < 0) {
 		return ret;
 	}
-	// TODO: read options
+
+	sd_bus_message_enter_container(msg, 'a', "{sv}");
+	if (ret < 0) {
+		return ret;
+	}
+	char *key;
+	int innerRet = 0;
+	while ((ret = sd_bus_message_enter_container(msg, 'e', "sv")) > 0) {
+		innerRet = sd_bus_message_read(msg, "s", &key);
+		if (innerRet < 0) {
+			return innerRet;
+		}
+
+		if (strcmp(key, "interactive") == 0) {
+			bool mode;
+			sd_bus_message_read(msg, "v", "b", &mode);
+			logprint(INFO, "dbus: option interactive: %x", mode);
+			interactive = mode;
+		} else if (strcmp(key, "modal") == 0) {
+			bool modal;
+			sd_bus_message_read(msg, "v", "b", &modal);
+			logprint(INFO, "dbus: option modal: %x", modal);
+		} else {
+			logprint(WARN, "dbus: unknown option %s", key);
+			sd_bus_message_skip(msg, "v");
+		}
+
+		innerRet = sd_bus_message_exit_container(msg);
+		if (innerRet < 0) {
+			return innerRet;
+		}
+	}
+	if (ret < 0) {
+		return ret;
+	}
+	ret = sd_bus_message_exit_container(msg);
+	if (ret < 0) {
+		return ret;
+	}
 
 	// TODO: cleanup this
 	struct xdpw_request *req =
@@ -54,9 +181,25 @@ static int method_screenshot(sd_bus_message *msg, void *data,
 		return -ENOMEM;
 	}
 
+	char *coordinates;
+	if (interactive) {
+		coordinates = exec_coordinates_selector();
+		if (coordinates == NULL || !coordinates[0]) {
+			free(coordinates);
+			return -1;
+		}
+	}
+
 	// TODO: choose a better path
 	const char path[] = "/tmp/out.png";
-	if (!exec_screenshooter(path)) {
+	if (interactive && !exec_screenshooter_with_coordinates(path, coordinates)) {
+		free(coordinates);
+		return -1;
+	}
+	if (interactive) {
+		free(coordinates);
+	}
+	if (!interactive && !exec_screenshooter(path)) {
 		return -1;
 	}
 
